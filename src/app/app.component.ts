e6858d7a9a185e7b0dbee5dfbb804e23b9ee7a1d
Fabian Czapski <czapskifabian@gmail.com>
2017-10-02 22:54:56 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Component, OnInit } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  constructor(private http: Http) {
+  }
+
+  public ngOnInit(): void {
+    this.getServers()
+    .subscribe(
+        (servers: any[]) => console.log(servers),
+        (error) => console.log(error)
+      );
+    console.log(this.getServers());
+  }
+
+  getServers() {
+    return this.http.get('http://localhost:56750/api/values')
+      .map(
+        (response: Response) => {
+          const data = response.json();
+          return data;
+        }
+      )
+      .catch(
+        (error: Response) => {
+          console.log(error);
+          return Observable.throw('Something went wrong');
+        }
+      );
+  }
 }
